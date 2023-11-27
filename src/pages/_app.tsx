155553d5ae4a91499ced3410d89c91ef82355698
Ivan Vasilev <20002mc@gmail.com>
2023-11-27 16:42:22 +0300

    add navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,24 @@
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import "../app/globals.css"
-export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+
+export default function MyApp({Component, pageProps}: AppProps) {
+    return <>
+       <Navbar/>
+        <Component {...pageProps} />
+    </>
+}
+
+function Navbar() {
+  return <nav className="bg-white shadow dark:bg-gray-800">
+      <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+          <a href="/" className="text-gray-800 dark:text-gray-200 mx-1.5 sm:mx-6">Main</a>
+
+          <a href="/catalog" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">catalog</a>
+
+          <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">forum</a>
+
+          <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">blog</a>
+
+      </div>
+  </nav>
 }
