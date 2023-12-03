63a8ed9d4b270944ff3b12f7a6936a30cd5a305c
Ivan Vasilev <20002mc@gmail.com>
2023-12-03 14:21:02 +0300

    improve upload file component

--- src/pages/profile/[userId]/edit/index.tsx
+++ src/pages/profile/[userId]/edit/index.tsx
@@ -2,7 +2,7 @@ import {GetServerSidePropsContext} from "next";
 import {fetchFormData, fetchOr404} from "@/common/fetch";
 import {Profile, ProfileEncoded} from "@/dto/user";
 import {HOST} from "@/app/globals";
-import {ChangeEvent, useState, MouseEvent} from "react";
+import {ChangeEvent, useState, MouseEvent, ReactNode} from "react";
 import {useRouter} from "next/router";
 
 const inputClasses = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
@@ -51,10 +51,8 @@ export default function EditProfile(props: ProfileEncoded) {
     function handleStatus(e: ChangeEvent<HTMLInputElement>) {
         setBio({...bio, status: e.target.value})
     }
-    function handlePic(e: ChangeEvent<HTMLInputElement>) {
-        const f = e.target?.files
-        if (!f) return
-        setPic(f[0] || null)
+    function handlePic(f: File) {
+        setPic(f)
     }
 
     async function updateProfile(e: MouseEvent) {
@@ -109,28 +107,7 @@ export default function EditProfile(props: ProfileEncoded) {
                         </div>
 
                     </div>
-                    <div className="flex justify-center mt-8">
-                        <div className="max-w-2xl rounded-lg shadow-xl bg-gray-50">
-                            <div className="m-4">
-                                <label className="inline-block mb-2 text-gray-500">Upload profile pic</label>
-                                <div className="flex items-center justify-center w-full">
-                                    <label
-                                        className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
-                                        <div className="flex flex-col items-center justify-center pt-7">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
-                                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                                            </svg>
-                                            <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
-                                                Attach a file</p>
-                                        </div>
-                                        <input type="file" className="opacity-0" onChange={handlePic}/>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   <UploadImageWithPreview pic={props.picture} setFile={handlePic} file={pic}/>
                     <div className="flex items-center space-x-4 mt-2.5">
                         <button className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={updateProfile}>
                             Update profile
@@ -141,6 +118,48 @@ export default function EditProfile(props: ProfileEncoded) {
                         </button>
                     </div>
                 </form>
+            </div>
+        </div>
+    </div>
+}
+
+function UploadImageWithPreview(props: {
+    pic: string
+    setFile: (f: File) => any
+    file: File | null
+}) {
+    function handlePic(e: ChangeEvent<HTMLInputElement>) {
+        const f = e.target?.files
+        if (!f) return
+        props.setFile(f[0])
+    }
+
+    let src: string = `${HOST}/static/${props.pic}`
+    if (props.file) {
+        src = URL.createObjectURL(props.file)
+    }
+
+    return  <div className="flex justify-center mt-8">
+        <div className="max-w-2xl rounded-lg shadow-xl bg-gray-50">
+            <div className="m-4">
+                <label className="inline-block mb-2 text-gray-500">Upload profile pic</label>
+                {(props.pic) ? <img src={src} alt=""/> : <>
+                    <div className="flex items-center justify-center w-full">
+                        <label
+                            className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                            <div className="flex flex-col items-center justify-center pt-7">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                                </svg>
+                                <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                                    Attach a file</p>
+                            </div>
+                        </label>
+                    </div>
+                </>}
+                <input type="file" className="" onChange={handlePic}/>
             </div>
         </div>
     </div>
