d3fbdab16f2cc536d0ac3fa0bf5c48feb6f6cabf
Ivan Vaslilev <20002mc@gmail.com>
2024-01-11 18:14:05 +0300

    add server addr for client

--- src/app/globals.tsx
+++ src/app/globals.tsx
@@ -1,3 +1,3 @@
-export const HOST = process.env["NEXT_PUBLIC_SERVER_ADDR"] || ""
+export const HOST = process.env["SERVER_ADDR"] || process.env["NEXT_PUBLIC_SERVER_ADDR"] || ""
 export const DEFAULT_MANGA_THUMB = `${HOST}/static/default/thumbnail.jpg`
 export const DEFAULT_PROFILE_PIC = `${HOST}/static/default/picture.jpeg`
